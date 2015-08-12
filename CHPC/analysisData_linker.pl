66c923fec2f358a3edefb6fe960dd61e6949cf6c
srynobio <shawn.rynearson@gmail.com>
2015-08-12 14:20:17 -0600

    added updated to analysisData_linker

--- CHPC/analysisData_linker.pl
+++ CHPC/analysisData_linker.pl
@@ -12,38 +12,50 @@ use Heimdall;
 my $usage = "
 
 Synopsis:
+    perl analysisData_linker.pl -list_projects 
     perl analysisData_linker.pl -analysis_id A76
     perl analysisData_linker.pl -analysis_id A76 -output_dir CDH_Project
 
 Description:
     Will search the Lustre Repository for GNomEx AnalysisData ids and create a symlink.
+
+    Using the -list_project option will list all available UCGD/UGP projects.
     
 Required options:
-    -analysis_id, -ai   GNomEx AnalysisData id 
+    -analysis_id    GNomEx AnalysisData id 
 
 Additional options:
-    -output_dir, -od    Path and name of the directory to name link. [default: current, -analysis_id].
+    -output_dir     Path and name of the directory to name link. [default: current, -analysis_id].
+    -list_projects  Will output table of all current CHPC projects and analysis ids.
 
 \n";
 
-my ($analysis_id, $output);
-GetOptions( 
-    "analysis_id|ai=s" => \$analysis_id, 
-    "output_dir|od=s"  => \$output,
+my ( $analysis_id, $list, $output );
+GetOptions(
+    "analysis_id=s" => \$analysis_id,
+    "list_projects" => \$list,
+    "output_dir"    => \$output,
 );
-die $usage if ( !$analysis_id );
 
 ## make object for record keeping.
-my $watch = Heimdall->new();
+my $watch  = Heimdall->new();
 my $whoami = `whoami`;
 chomp $whoami;
-
 
 ## Default to current.
 $output //= '.';
 
 ## location of all GNomEx analysis data.
 my @data_dir = ('/scratch/ucgd/lustre/Repository/AnalysisData');
+
+## if user just wants to list projects
+if ($list) {
+    list_projects();
+    exit(0);
+}
+
+## before moving on check id
+die $usage if ( !$analysis_id );
 
 my $found;
 find( { bydepth => 1, no_chdir => 1, wanted => \&analysis_locate }, @data_dir );
@@ -61,12 +73,32 @@ else {
 
 ##------------------------------------------------##
 
+sub project_locate {
+    my $dir = $File::Find::name;
+    say $dir;
+}
+
+##------------------------------------------------##
+
 sub analysis_locate {
     no warnings;
     my $dir = $File::Find::name;
     next unless ( $dir =~ /$analysis_id$/ );
 
     $found = $dir;
+}
+
+##------------------------------------------------##
+
+sub list_projects {
+    die "$0: analysis_id_name.txt file not found."
+      if ( !-e 'analysis_id_name.txt' );
+
+    say "";
+    say "| Project Name | Analysis ID |";
+    say "| -------------|------------ |";
+    system("column -t analysis_id_name.txt");
+    say "";
 }
 
 ##------------------------------------------------##
