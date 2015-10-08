06e7e3a6b9caa59e75494fe89d313739eeb9b2b0
srynobio <shawn.rynearson@gmail.com>
2015-10-07 21:48:18 -0600

    updated nantomics scripts

--- bin/nantomics/nantomics_data_run_to_GVCF.pl
+++ bin/nantomics/nantomics_data_run_to_GVCF.pl
@@ -27,17 +27,15 @@ my $cfg     = $watch->config->{nantomics_transfer}->{cfg};
 ## make directory & run there.
 my $date = localtime;
 $date =~ s/\s+/_/g;
-my $dir ='UGPp_Run_' . $date;
+my $dir = 'UGPp_Run_' . $date;
 
 mkdir $dir;
 chdir $dir;
 
-say `pwd`;
 my $cmd = sprintf( 
     "nohup %s -cfg %s -il %s -ql 60 -e cluster --run",
-    $ugpp, $cfg, $region 
+    $ugpp, $cfg, $region
 );
-say $cmd;
 
 $watch->info_log("$0: pipeline processing started.");
 eval { system($cmd) };
@@ -45,6 +43,4 @@ eval { system($cmd) };
 if ($@) {
     $watch->error_log("$0: issue execute pipeline process");
 }
-
 $watch->info_log("$0: pipeline processing finished.");
-
