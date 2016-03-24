88ea093d0c9df941a726342af5a3b0974cb5055b
shawn rynearson <shawn.rynearson@gmail.com>
2016-03-24 12:34:53 -0600

    code updates to UGP tasks

--- lib/UGP/Tasks/__module__.pm
+++ lib/UGP/Tasks/__module__.pm
@@ -395,34 +395,3 @@ sub _analysis_build_update_db {
 
 1;
 
-=pod
-
-=head1 NAME
-
-$::module_name - {{ SHORT DESCRIPTION }}
-
-=head1 DESCRIPTION
-
-{{ LONG DESCRIPTION }}
-
-=head1 USAGE
-
-{{ USAGE DESCRIPTION }}
-
- include qw/UGP::Tasks/;
-
- task yourtask => sub {
-    UGP::Tasks::example();
- };
-
-=head1 TASKS
-
-=over 4
-
-=item example
-
-This is an example Task. This task just output's the uptime of the system.
-
-=back
-
-=cut
